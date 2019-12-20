23f71aa3c6d25b7e286e6a61dea78c88859ab763
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-20 13:23:38 +0100

    cambiamenti3

--- CompitoVacanze/Program.cs
+++ CompitoVacanze/Program.cs
@@ -9,12 +9,16 @@ namespace CompitoVacanze
             int a = int.MaxValue;
             //viene stampato il numero massimo che un int può contenere 
             Console.WriteLine(a);
-            //il programmma va int overflow perchè viene aggiunto un 1 al numero massimo che l'int può contenere 
-            Console.WriteLine(a + 1);
+            if (a == 214783647)
+            {
+                //il programmma va in overflow perchè viene aggiunto un 1 al numero massimo che l'int può contenere 
+                Console.WriteLine(a + 1);
+            }
             int b = 128;
             int c = -b;
             Console.WriteLine(Convert.ToString(b, 2));
             Console.WriteLine(Convert.ToString(c, 2));
+            //in queste 4 righe l'int c viene convertito in binario e poi stampato
             int value = 31;
             int somma = value;
             value = ~value + 1; // NOT
